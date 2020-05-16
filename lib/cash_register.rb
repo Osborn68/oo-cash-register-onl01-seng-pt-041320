c3f1a46aebea83552cfdef97842b5457cd3ab4f9
Christopher Osborn <z68ozz@gmail.com>
2020-05-16 00:04:47 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
+require 'pry'
+class CashRegister
+  
+  attr_accessor :total, :discount, :items, :quantity
+  
+  def initialize(discount= 0)
+    @total = 0
+    @discount = discount
+    
+  end 
+  
+  def add_item(title, price, quantity = 1)
+    self.total += price * quantity
+   quantity.times do 
+     items << title 
+    end
+    self.last_transaction_amount = amount * quantity
+  end
+    
+  
+    
+  
+end
+
 
